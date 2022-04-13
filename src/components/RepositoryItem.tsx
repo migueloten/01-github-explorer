090550d3aaa78397b6aa13ac821ce1be4efe119f
Severino Miguel <severino.miguel.119@ufrn.edu.br>
2022-04-13 10:27:35 -0300

    :art: Atualizações de deoendencias do projeto

---
+++ src/components/RepositoryItem.tsx
@@ -0,0 +1,20 @@
+interface RepositoryItermProps {
+    repository: {
+        name: string;
+        description: string;
+        html_url: string;
+    }
+}
+
+export function RepositoryItem(props: RepositoryItermProps) {
+    return (
+        <li>
+            <strong>{props.repository.name}</strong>
+            <p>{props.repository.description}</p>
+
+            <a href={props.repository.html_url}>
+                Acessar repositório
+            </a>
+        </li>
+    );
+}
